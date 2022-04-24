0472c3609e2c1248db3dec8e10c5d07cd258efe1
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-04-24 11:31:09 -0300

    Videos 3 y 4

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string ContraseniaCorrecta;
+    string ContraseniaUsr;
+    Text IngresoUsr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        ContraseniaUsr = IngresoUsr.text;
+    }
+}
